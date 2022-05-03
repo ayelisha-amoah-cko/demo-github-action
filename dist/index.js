0b0231b0ac80579752526703736e69ade6a11217
Ayelisha Amoah <ayelisha.amoah@checkout.com>
2022-05-03 13:13:21 +0100

    add debug logs

--- dist/index.js
+++ dist/index.js
@@ -6022,18 +6022,25 @@ const AVAILABLE_CHARACTERS = ["bender", "fry", "leela", "dr-zoidberg"];
 async function run() {
   const character = core.getInput("character") || DEFAULT_CHARACTER;
 
+  core.debug(`[Futurama] Input Character: ${character}`);
+
   if (!AVAILABLE_CHARACTERS.includes(character)) {
     core.setFailed(`Unknown character: ${character}`);
     return;
   }
 
-  console.log(character);
+  core.debug(`[Futurama] Retrieving quote for ${character}`);
 
   const response = await axios.get(
     `https://futuramaapi.herokuapp.com/api/characters/${character}/1`
   );
 
+  core.debug(`[Futurama] Successfully retrieved quote for ${character}`);
+
   const { data } = response;
+
+  core.debug(`[Futurama] Data: ${JSON.stringify(data)}`);
+  
   const firstEntry = data[0];
 
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
