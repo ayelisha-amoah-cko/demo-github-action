6883137e9a783359634ce6d2eca171a0c2bc2dbd
Ayelisha Amoah <ayelisha.amoah@checkout.com>
2022-05-03 12:42:19 +0100

    echo quote from outputs

--- src/action.js
+++ src/action.js
@@ -15,6 +15,8 @@ async function run() {
   const { data } = response;
   const firstEntry = data[0];
   console.log(`${firstEntry.character}: ${firstEntry.quote}`);
+
+  core.setOutput("quote", firstEntry);
 }
 
 run();
